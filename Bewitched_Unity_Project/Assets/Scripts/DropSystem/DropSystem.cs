f9c9a47d7f2853d50350858d9ebdc088e6053b7b
MohammedAlanizy <123278242+MohammedAlanizy@users.noreply.github.com>
2025-09-08 08:38:18 -0400

    Check the instance before adding health

--- Bewitched_Unity_Project/Assets/Scripts/DropSystem/DropSystem.cs
+++ Bewitched_Unity_Project/Assets/Scripts/DropSystem/DropSystem.cs
@@ -134,6 +134,7 @@ public class DropSystem : MonoBehaviour
     /// </summary>
     public void SalvageDrop()
     {
+        if (PlayerController.instance == null || PlayerController.instance.GetHag() == null)
         // Add specified amount of health to the player
         PlayerController.instance.GetHag().health.AddHealth(salvageAmount);
     }
