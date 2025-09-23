577237599c17bcc5a0ac80788090cbfe57a29f5a
zoiewithani1 <tsoizoie@msu.edu>
2025-09-22 20:36:24 -0400

    Adding vertical stacks (unfinished)
    pushing so i can switch computers..

--- Bewitched_Unity_Project/Assets/Scripts/UI/HUDManager.cs
+++ Bewitched_Unity_Project/Assets/Scripts/UI/HUDManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 public class HUDManager : MonoBehaviour
 {
     public static HUDManager Instance;
+
     [Header("Upgrades UI")]
     [Tooltip("Parent that holds the frame where the upgrades will go on the HUD.")]
-
     public Transform upgradeIconParent;
+
+    [Tooltip("Tracking by DropData ID")]
+    private Dictionary<string, Transform> upgradeDict = new Dictionary<string, Transform>();
 
     /// <summary>
     /// It sets the instance of the HUDManager class,
@@ -33,6 +36,22 @@ public class HUDManager : MonoBehaviour
     public void AddUpgrade(DropData upgrade)
     {
         if (upgrade == null) return;
+
+        string upgradeID = upgrade.GetID();
+        if (!upgradeDict.ContainsKey(upgradeID))
+        {
+            GameObject stack = new GameObject(upgrade.GetDropName() + "_Stack");
+            stack.transform.SetParent(upgradeIconParent, false);
+
+            VerticalLayoutGroup verticalStack = stack.AddComponent<VerticalLayoutGroup>();
+            verticalStack.childControlWidth = true;
+            verticalStack.childControlHeight = true;
+            verticalStack.childForceExpandWidth = false;
+            verticalStack.childForceExpandHeight = false;
+
+            upgradeDict[upgradeID] = stack.transform;
+        }
+
 
         GameObject upgradeObject = new GameObject(upgrade.GetDropName());
         upgradeObject.transform.SetParent(upgradeIconParent, false);
