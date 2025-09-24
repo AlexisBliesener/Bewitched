8c3327313c9ce471f33a0d8ccb3701b685cb9112
zoiewithani1 <tsoizoie@msu.edu>
2025-09-24 18:00:25 -0400

    Adding Controller Support (main menu, pause, upgrade selection, swap upgrades), adding in a death screen (not functional)
    Currently has the pause/upgrade bug

--- Bewitched_Unity_Project/Assets/Scripts/UI/PauseManager.cs
+++ Bewitched_Unity_Project/Assets/Scripts/UI/PauseManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 
 public class PauseManager : MonoBehaviour
 {
@@ -15,17 +17,35 @@ public class PauseManager : MonoBehaviour
     public GameObject compendiumScreen;
     [Tooltip("The Upgrade Log Screen")]
     public GameObject upgradeScreen;
+    [Tooltip("The Upgrade Selection Screen")]
+    public GameObject upgradeSelectionUI;
+    [Tooltip("The Swap Upgrade Screen")]
+    public GameObject swapUpgradeUI;
+
+    [Header("First Selected Buttons")]
+    [Tooltip("The first button to be selected when Pause menu is opened.")]
+    public GameObject pauseButton;
+    [Tooltip("The first button to be selected when Compendium menu is opened.")]
+    public GameObject compendiumButton;
+    [Tooltip("The first button to be selected when Upgrade Log menu is opened.")]
+    public GameObject upgradeButton;
+    [Tooltip("The first button to be selected when Settings menu is opened.")]
+    public GameObject settingsButton;
 
     private void OnEnable()
     {
         OpenScreen(mainPauseScreen);
+        EventSystem.current.SetSelectedGameObject(pauseButton);
         Cursor.lockState = CursorLockMode.None;
     }
 
     private void OnDisable()
     {
-        Time.timeScale = 1.0f;
-        Cursor.lockState = CursorLockMode.Locked;
+        if (!upgradeSelectionUI.activeSelf && !swapUpgradeUI.activeSelf)
+        {
+            Time.timeScale = 1.0f;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     public void CloseAllScreens()
@@ -34,12 +54,32 @@ public class PauseManager : MonoBehaviour
         settingsScreen.SetActive(false);
         compendiumScreen.SetActive(false);
         upgradeScreen.SetActive(false);
+        if (upgradeSelectionUI.activeSelf)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 
     public void OpenScreen(GameObject screen)
     {
         CloseAllScreens();
         screen.SetActive(true);
+        if (screen.name == "PauseMain")
+        {
+            EventSystem.current.SetSelectedGameObject(pauseButton);
+        }
+        else if (screen.name == "Compendium")
+        {
+            EventSystem.current.SetSelectedGameObject(compendiumButton);
+        }
+        else if (screen.name == "UpgradeLog")
+        {
+            EventSystem.current.SetSelectedGameObject(upgradeButton);
+        }
+        else if (screen.name == "Settings")
+        {
+            EventSystem.current.SetSelectedGameObject(settingsButton);
+        }
     }
 
     public void QuitToDesktop()
