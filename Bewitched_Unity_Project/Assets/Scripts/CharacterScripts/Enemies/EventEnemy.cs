d05d2a9e22f3353753fd62c627d3518a47432105
MohammedAlanizy <123278242+MohammedAlanizy@users.noreply.github.com>
2025-10-12 03:36:28 -0400

    Changed EventEnemy to work with ogre

---
+++ Bewitched_Unity_Project/Assets/Scripts/CharacterScripts/Enemies/EventEnemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is the event enemy for the event system
+public class EventEnemy : MonoBehaviour
+{
+    [Tooltip("The enemy that is controlled by this event enemy")]
+    private Enemy enemyForEvent;
+    /// <summary>
+    /// The enum for the enemy state
+    /// </summary>
+    public enum EventEnemyState
+    {
+        Attacking,
+        Dizzy,
+        Possessed,
+    }
+    [Tooltip("The current state of the enemy")]
+    private EventEnemyState enemyState = EventEnemyState.Attacking;
+    private void Start()
+    {
+        enemyForEvent = GetComponent<Enemy>();
+        if (enemyForEvent == null)
+        {
+            Debug.LogWarning("Enemy not found on event enemy");
+        }
+    }
+    /// <summary>
+    /// Sets the state of the enemy
+    /// </summary>
+    public void SetState(EventEnemyState state)
+    {
+        enemyState = state;
+        if (enemyForEvent == null)
+        {
+            Debug.LogWarning("Enemy not found on event enemy");
+            return;
+        }
+        switch (state)
+        {
+            case EventEnemyState.Attacking:
+                Debug.Log("Attacking");
+                enemyForEvent.SetControlled(false);
+                break;
+            case EventEnemyState.Dizzy:
+                Debug.Log("Dizzy");
+                enemyForEvent.SetControlled(true);
+                break;
+            case EventEnemyState.Possessed:
+                break;
+        }
+    }
+    /// <summary>
+    /// Gets the state of the enemy
+    /// </summary>
+    public EventEnemyState GetState()
+    {
+        return enemyState;
+    }
+    /// <summary>
+    /// Gets the enemy controlled by this event enemy
+    /// </summary>
+    public Enemy GetEnemy()
+    {
+        return enemyForEvent;
+    }
+
+}
