fb183db589f4f911d11a86074220bcc37f22fe37
MohammedAlanizy <123278242+MohammedAlanizy@users.noreply.github.com>
2025-10-27 20:42:58 -0400

    Fix ogre sometimes doesn't move ...

--- Bewitched_Unity_Project/Assets/Scripts/CharacterScripts/Enemies/Ogre.cs
+++ Bewitched_Unity_Project/Assets/Scripts/CharacterScripts/Enemies/Ogre.cs
@@ -88,7 +88,7 @@ public class Ogre : Enemy
         if (dead || lobotimzed) return;
         ManageSurrounding();
         currentPlayer = playerController.GetCurrentCharacter();
-        SetAIState();
+        // SetAIState();
         SetBehavior();
         CreateLocalInvalidArea();
     }
@@ -259,7 +259,9 @@ public class Ogre : Enemy
 
         lockedCharacter = null;
         attackingPrimary = false;
+        attackStateCoroutine = null;
         timeLastPrimary = Time.time;
+        aiState = AIMovementState.Chasing;
     }
 
     /// <summary>
