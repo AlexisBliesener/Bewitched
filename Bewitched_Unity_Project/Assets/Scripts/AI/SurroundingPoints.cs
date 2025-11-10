7ad53b91df075007fa1be8e51f85a4056e3a1bff
Anders Nyland <163351353+NylandA1@users.noreply.github.com>
2025-11-10 14:39:33 -0500

    Removed another NavMesh Reference (BOOOOOOOOO)

--- Bewitched_Unity_Project/Assets/Scripts/AI/SurroundingPoints.cs
+++ Bewitched_Unity_Project/Assets/Scripts/AI/SurroundingPoints.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.AI;
 
 /// <summary>
 /// A class for creating points for AI to try to reach
