83bcad25f14c91ee1db8e7673a96cae3aa2b958b
Anders Nyland <163351353+NylandA1@users.noreply.github.com>
2025-11-16 22:39:13 -0500

    Animator clamping

--- Bewitched_Unity_Project/Assets/Scripts/CharacterScripts/Enemies/Goblin.cs
+++ Bewitched_Unity_Project/Assets/Scripts/CharacterScripts/Enemies/Goblin.cs
@@ -125,7 +125,7 @@ public class Goblin : Enemy
         currentPlayer = playerController.GetCurrentCharacter();
 
         SetDebugString();
-        if (!playerControlling) Debug.Log(debugAIInfo);
+        //if (playerControlling) Debug.Log(debugAIInfo);
 
         SetAIState();
 
