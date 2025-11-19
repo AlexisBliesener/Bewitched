da6efc8d686d4f502036ca8e5e9679e87bffc297
MohammedAlanizy <123278242+MohammedAlanizy@users.noreply.github.com>
2025-11-19 18:20:29 -0500

    fix possess

--- Bewitched_Unity_Project/Assets/Scripts/AbilityStuff/Hag/PossessionAbility.cs
+++ Bewitched_Unity_Project/Assets/Scripts/AbilityStuff/Hag/PossessionAbility.cs
@@ -606,6 +606,7 @@ public class PossessionAbility : MonoBehaviour
             PriorityQueue<(float, Character)> distances = new PriorityQueue<(float, Character)>();
             foreach (Character character in possessionColliderScript.GetCharactersInPossession())
             {
+                if (character.canPossess == false) continue;
                 Vector3 toCharacter = new Vector3( character.transform.position.x, 0, character.transform.position.z) - new Vector3(currentCharacter.transform.position.x, 0, currentCharacter.transform.position.z);
 
                 toCharacter = toCharacter.normalized;
