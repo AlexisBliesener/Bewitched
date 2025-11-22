7cb453ce9d92193f5a4a91b0e5c940fcbe9d154b
MohammedAlanizy <123278242+MohammedAlanizy@users.noreply.github.com>
2025-11-21 21:19:23 -0500

    GetOffOfMe upgrade

---
+++ Bewitched_Unity_Project/Assets/Scripts/DropSystem/DropInstances/GetOffOfMe.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handles the "mew" upgrade,
+/// increasing damage dealt to enemies when swapping to them 
+/// </summary>
+public class GetOffOfMe : MonoBehaviour, IDrop
+{
+    const string FILE_ENDING = ".json";
+
+    [Tooltip("Singleton")]
+    public static GetOffOfMe instance;
+
+    [Tooltip("The amount of stacks this upgrade has")]
+    public int stackNum { get; set; }
+
+    [SerializeField, Tooltip("The damage multiplier per stack when swapping enemies")]
+    private float[] knockBackMultiplier = { 1.25f, 1.5f, 1.75f };
+    [Tooltip("Whether the effect is currently active")]
+    private bool active = false;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    #region Saving/Loading
+
+    [ContextMenu("Save to JSON")]
+    public void SaveToJson()
+    {
+        string statsStr = JsonUtility.ToJson(this, true);
+
+        string folderPath = Path.Combine(Application.dataPath, "JSON");
+        folderPath = Path.Combine(folderPath, "UpgradeStats");
+        SeeFilePath();
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        string filePath = Path.Combine(folderPath, nameof(GetOffOfMe) + FILE_ENDING);
+        File.WriteAllText(filePath, statsStr);
+
+
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+#endif
+
+
+    }
+
+    [ContextMenu("See File Path")]
+    public void SeeFilePath()
+    {
+        string folderPath = Path.Combine(Application.persistentDataPath, "JSON");
+        folderPath = Path.Combine(folderPath, "UpgradeStats");
+        Debug.Log("Path To JSON File:");
+        Debug.Log(folderPath);
+    }
+
+    [ContextMenu("Load From JSON")]
+    public void LoadFromJson()
+    {
+
+        string folderPath = Path.Combine(Application.dataPath, "JSON");
+        folderPath = Path.Combine(folderPath, "UpgradeStats");
+        string filePath = Path.Combine(folderPath, nameof(GetOffOfMe) + FILE_ENDING);
+
+        string jsonStr = File.ReadAllText(filePath);
+
+        string[] jsons = jsonStr.Split("|");
+
+        JsonUtility.FromJsonOverwrite(jsons[0], this);
+
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+#endif
+
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Activates the GetOffOfMe effect
+    /// </summary>
+    /// <param name="dropData">The drop data of the upgrade</param>
+    public void Activate(DropData dropData = null)
+    {
+        active = true;
+    }
+    /// <summary>
+    /// Deactivates the GetOffOfMe effect
+    /// </summary>
+    public void Deactivate()
+    {
+        active = false;
+    }
+    /// <summary>
+    /// Get the modified knockback for the GetOffOfMe upgrage
+    /// </summary>
+    /// <param name="baseKnockback">The base knockback to apply</param>
+    /// <returns>The modified knockback</returns>
+    public float GetModifiedKnockback(float baseKnockback)
+    {
+        if (!active) return baseKnockback;
+    
+        // if the stack is greater than the length of the array, fall back to the last multiplier
+        if (stackNum >= knockBackMultiplier.Length) 
+            return baseKnockback * knockBackMultiplier[knockBackMultiplier.Length - 1];
+
+        // otherwise, return the multiplier at the current stack
+        return baseKnockback * knockBackMultiplier[stackNum];
+    }
+}
