b5f6265c3f8cec75ee683f4fe795d57a314f79e3
AlexisBliesener <bliesen7@msu.edu>
2025-11-29 21:59:15 -0500

    goblin animation mask

--- Bewitched_Unity_Project/Assets/Scripts/AnimatorScripts/GoblinAnimator.cs
+++ Bewitched_Unity_Project/Assets/Scripts/AnimatorScripts/GoblinAnimator.cs
@@ -146,7 +146,41 @@ public class GoblinAnimator : CharacterAnimator
 
         animator.SetInteger("PrimaryCombo", currentPrimaryComboStep);
 
+        if (newState == "Idle")
+        {
+            legsRunning = false;
+            animator.ResetTrigger("LegsRun");
+            animator.SetTrigger("LegsIdle");
+        }
+        else if (newState == "Run")
+        {
+            legsRunning = true;
+            animator.ResetTrigger("LegsIdle");
+            animator.SetTrigger("LegsRun");
+        }
+
         SwitchState(newState);
+    }
+
+    private bool legsRunning = false;
+    private float legLayerWeight = 0f;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (legsRunning && (currentAnimationState == "Hit"))
+        {
+            legLayerWeight += Time.deltaTime * 5;
+            if (legLayerWeight > 1) legLayerWeight = 1;
+            animator.SetLayerWeight(1, legLayerWeight);
+        }
+        else
+        {
+            legLayerWeight -= Time.deltaTime * 5;
+            if (legLayerWeight < 0) legLayerWeight = 0;
+            animator.SetLayerWeight(1, legLayerWeight);
+        }
     }
 
     /// <summary>
